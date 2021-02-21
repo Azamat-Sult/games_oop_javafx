6bee1344d9d273d4e6de5a1118790bacda3c79e4
Azamat-Sult <asultangareev@gmail.com>
2021-02-21 12:06:33 +0500

    2. Сокобан. Тестовое задание. [#464921] Исправление 1

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,7 +5,7 @@ public class Win {
     public static boolean checkRow(int[][] board1, int row) {
         boolean rsl = true;
         for (int index1 = 0; index1 < board1.length; index1++) {
-            if (board1[index1][row] != 1) {
+            if (board1[row][index1] != 1) {
                 rsl = false;
                 break;
             }
@@ -16,7 +16,7 @@ public class Win {
     public static boolean checkColumn(int[][] board1, int column) {
         boolean rsl = true;
         for (int index1 = 0; index1 < board1.length; index1++) {
-            if (board1[column][index1] != 1) {
+            if (board1[index1][column] != 1) {
                 rsl = false;
                 break;
             }
@@ -27,8 +27,8 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int index1 = 0; index1 < board.length; index1++) {
-            if (board[index1][index1] == 1) {
-                rsl = checkColumn(board, index1) || checkRow(board, index1);
+            if (checkColumn(board, index1) || checkRow(board, index1)) {
+                rsl = true;
                 break;
             }
         }
